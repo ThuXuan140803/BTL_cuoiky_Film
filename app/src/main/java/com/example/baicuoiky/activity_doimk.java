7ddc1029c60d159efb297f3645e785e3edc94cec
ThuXuan140803 <162014199+ThuXuan140803@users.noreply.github.com>
2024-03-22 22:19:33 +0700

    first commit

---
+++ app/src/main/java/com/example/baicuoiky/activity_doimk.java
@@ -0,0 +1,14 @@
+package com.example.baicuoiky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_doimk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doimk);
+    }
+}
